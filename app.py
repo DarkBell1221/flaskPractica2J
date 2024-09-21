ada22ad4d811ce131fec086f9c61915178f9a9f5
DarkBell1221 <belldark74@gmail.com>
2024-09-20 20:14:49 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,78 @@
+from flask import Flask
+from markupsafe import escape
+
+from flask import render_template
+from flask import request
+
+import pusher
+
+import mysql.connector
+import datetime
+import pytz
+
+con = mysql.connector.connect(
+  host="185.232.14.52",
+  database="u760464709_tst_sep",
+  user="u760464709_tst_sep_usr",
+  password="dJ0CIAFF="
+)
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    con.close()
+  
+    return render_template("app.html")
+
+@app.route("/alumnos")
+def alumnos():
+    con.close()
+  
+    return render_template("alumnos.html")
+
+@app.route("/alumnos/guardar", methods=["POST"])
+def alumnosGuardar():
+    con.close()
+  
+    matricula      = request.form["txtMatriculaFA"]
+    nombreapellido = request.form["txtNombreApellidoFA"]
+
+    return f"Matrícula: {matricula} Nombre y Apellido: {nombreapellido}"
+
+@app.route("/registrar", methods=["GET"])
+def registrar():
+    args = request.args
+    pusher_client = pusher.Pusher(
+      app_id="1714541",
+      key="cda1cc599395d699a2af",
+      secret="9e9c00fc36600060d9e2",
+      cluster="us2",
+      ssl=True
+    )
+
+    if not con.is_connected():
+        con.reconnect()
+    cursor = con.cursor()
+    
+    sql = "INSERT INTO sensor_log (Temperatura, Humedad, Fecha_Hora) VALUES (%s, %s, %s)"
+    val = (args["temperatura"], args["humedad"], datetime.datetime.now(pytz.timezone("America/Matamoros")))
+    cursor.execute(sql, val)
+
+    con.commit()
+    con.close()
+ 
+    pusher_client.trigger("registrosTiempoReal", "registroTiempoReal", args)
+    return args
+
+@app.route("/buscar")
+def buscar():
+    if not con.is_connected():
+        con.reconnect()
+    cursor = con.cursor()
+    cursor.execute("SELECT * FROM sensor_log ORDER BY Id_Log DESC")
+    registros = cursor.fetchall()
+
+    con.close()
+
+    return registros
